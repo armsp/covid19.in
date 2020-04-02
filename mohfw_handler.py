aa42fac8387d50cff064bdfd3dd30c848a062b50
armsp <shantamdps@gmail.com>
2020-04-02 23:56:50 +0530

    fixed geocode function - logger was extraneous

--- mohfw_handler.py
+++ mohfw_handler.py
@@ -29,7 +29,7 @@ def extract_clean_df(df):
     clean_df = df.head(-1)
     return clean_df
 
-def geocode(city, logger):
+def geocode(city):
     url = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates'
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
 
