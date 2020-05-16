2065cac0350620051fef4feaf1e3d5fa63864066
armsp <shantamdps@gmail.com>
2020-05-16 11:41:16 +0530

    fixing mobile issues

--- chloropleth.py
+++ chloropleth.py
@@ -44,6 +44,9 @@ def make_chloropleth_json(clean_state_dataset_path):
 
     final_map = (base+choro).configure_view(
         strokeWidth=0
+    ).configure_legend(
+        orient='bottom',
+        gradientLength = 500
     )
     # kwargs = {'actions': False}
     return final_map.to_json(indent=None)
